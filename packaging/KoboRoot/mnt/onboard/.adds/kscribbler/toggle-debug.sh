f79b0f6d759f3f405d605a51649e504965bd50b9
Young, Gianni <cmancy@tutanota.com>
2025-08-30 08:22:44 -0500

    fix: command output timeout was too long

--- packaging/KoboRoot/mnt/onboard/.adds/kscribbler/toggle-debug.sh
+++ packaging/KoboRoot/mnt/onboard/.adds/kscribbler/toggle-debug.sh
@@ -4,6 +4,6 @@ if [ -f "$KSDEBUG" ]; then
   rm -f $KSDEBUG
 else
   echo showing debug options
-  echo -e "menu_item:main:Kscribbler Init DB (no upload):cmd_output:60000:/opt/bin/kscribbler --init >> /mnt/onboard/.adds/kscribbler/kscribbler.log 2>&1" >> $KSDEBUG
-  echo -e "menu_item:main:Kscribbler Mark All Quotes as Uploaded (no upload):cmd_output:60000:/opt/bin/kscribbler --mark-all-as-uploaded >> /mnt/onboard/.adds/kscribbler/kscribbler.log 2>&1" >> $KSDEBUG
+  echo -e "menu_item:main:Kscribbler Init DB (no upload):cmd_output:9999:/opt/bin/kscribbler --init 2>&1| tee -a /mnt/onboard/.adds/kscribbler/kscribbler.log" >> $KSDEBUG
+  echo -e "menu_item:main:Kscribbler Mark All Quotes as Uploaded (no upload):cmd_output:9999:/opt/bin/kscribbler --mark-all-as-uploaded 2>&1 | tee -a /mnt/onboard/.adds/kscribbler/kscribbler.log" >> $KSDEBUG
 fi
